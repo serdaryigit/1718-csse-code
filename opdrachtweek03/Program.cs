3d38ac276be961eb09cfc04e370f33e76415100f
serdaryigit <serdyigi@student.arteveldehs.be>
2017-10-17 12:00:48 +0200

    Commit1-Vermenigvuldiging

---
+++ opdrachtweek03/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace opdrachtweek03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+    //vermenigvuldingstafel
+        
+       for (int i = 1; i <= 7; i++)
+     {
+          for (int j = 0; j <= 10; j++)
+          {
+            Console.WriteLine("{0} x {1}={2}", j, i, i*j);
+         }
+       }
+        Console.ReadLine();
+
+        }
+    }
+}
