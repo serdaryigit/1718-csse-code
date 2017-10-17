8edbaadb36c93751064422f7385d2c877617f130
serdaryigit <serdyigi@student.arteveldehs.be>
2017-10-17 12:44:45 +0200

    Commit2

--- opdrachtweek03/Program.cs
+++ opdrachtweek03/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 namespace opdrachtweek03
 {
@@ -6,17 +7,30 @@ namespace opdrachtweek03
     {
         static void Main(string[] args)
         {
+            
+        /// Vermenigvuldingstafel zonder input
+        //  for (int i = 1; i <= 7; i++)
+        //  {
+        //      for (int j = 0; j <= 10; j++)
+        //      {
+        //          Console.WriteLine("{0} x {1} = {2}", j, i, i*j);
+        //      }
+        //  }
+        //  Console.ReadLine();
 
-    //vermenigvuldingstafel
-        
-       for (int i = 1; i <= 7; i++)
-     {
-          for (int j = 0; j <= 10; j++)
-          {
-            Console.WriteLine("{0} x {1}={2}", j, i, i*j);
-         }
-       }
-        Console.ReadLine();
+
+        /// Vermenigvuldigingstafel met input
+
+        Console.WriteLine("Voer een getal in:");
+        int getal = int.Parse(Console.ReadLine());
+
+        string resultaat = "";
+
+        for (int i = 0; i < 11; i++)
+        {
+        resultaat += ($"{i.ToString()} x {getal.ToString()} = {getal * i} \n");
+        }
+        Console.WriteLine(resultaat);
 
         }
     }
