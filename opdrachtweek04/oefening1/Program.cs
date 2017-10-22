3bfb075bcee273d107d6b6d2d1ad344ccee44688
serdaryigit <serdyigi@student.arteveldehs.be>
2017-10-22 23:37:27 +0200

    oef1+oef2
    opdrachtweek4

---
+++ opdrachtweek04/oefening1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace oefening1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            // Oefening 1: Priemgetal
+            // Geef een getal in naar keuze klener dan 100
+            // Overlopen van 1 tot gekozen getal
+            // Priemgetal(deelbaar door 1 en zichzelf)
+
+            Console.Write("Geef een getal in van 0 tot 100: ");
+			int input = Convert.ToInt32(Console.ReadLine());
+			for(var x = 0; x <= input; x++)
+			{
+				int deelbaar = 0;
+                for (int i = 2; i <= x; i++)
+                if (x % i == 0)                        
+                deelbaar++;
+
+                if (x != 1 && deelbaar == 1){
+                Console.WriteLine(x + " is een priemgetal.");
+                }
+                else{ 
+                    Console.WriteLine(x + " is geen priemgetal.");
+                }
+			}
+        }
+    }
+}
---
+++ opdrachtweek04/oefening2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+
+namespace oefening2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //Oefening 2: Rijen van Fibonacci
+            
+            Console.Write("Geef een getal in: ");
+            int reeks = Convert.ToInt32(Console.ReadLine());
+
+            int fib1 = 0;
+            int fib2 = 1;
+
+            Console.WriteLine(fib1);
+            Console.WriteLine(fib2);
+
+            for (int i = 3; i <= reeks; i++)
+            {
+                int fib3 = fib1 + fib2;
+                fib1 = fib2;
+                fib2 = fib3;
+                Console.WriteLine(fib3);
+
+            }          
+        }
+    }
+}
