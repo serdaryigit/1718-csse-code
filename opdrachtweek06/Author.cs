bc9fb42d16d3a0fbff02d5ec70da38c9a44824ae
Unknown <serdyigi@student.arteveldehs.be>
2017-11-16 12:19:51 +0100

    opdrWeek6
    opdrachten

---
+++ opdrachtweek06/Author.cs
@@ -0,0 +1,16 @@
+namespace opdrachtweek6
+{
+   public class Author : Books
+    {
+        private string name;
+		
+        private string email;
+
+        private string gender;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
